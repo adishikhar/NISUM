db6d16848d5af1875c4143fe830597191def6501
Aditya Shikhar <adityashikhar7@gmail.com>
2025-05-13 11:49:04 +0530

    .

--- Sprint1_Day2/Q13.java
+++ Sprint1_Day2/Q13.java
@@ -1,5 +1,18 @@
 package Sprint1_Day2;
 
-public class Q13 {
-    
+class check {
+    static {
+        System.out.println("Static Block is executed");
+    }
+
+    static void print() {
+        System.out.println("Static Method is executed");
+    }
 }
+
+public class Q13 {
+    public static void main(String[] args) {
+        check.print();
+    }
+}
+
