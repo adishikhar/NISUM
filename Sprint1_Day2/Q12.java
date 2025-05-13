6aadd5602214bad5e2f2c20cad12e5f31a661a04
Aditya Shikhar <adityashikhar7@gmail.com>
2025-05-13 11:36:37 +0530

    .

---
+++ Sprint1_Day2/Q12.java
@@ -0,0 +1,18 @@
+package Sprint1_Day2;
+
+class Test {
+    int number = 10; // instance variable
+
+    static void show() {
+        // System.out.println(number); ❌ Error: can't access directly
+        Test obj = new Test();
+        System.out.println("Accessing via object: " + obj.number); // ✅
+    }
+}
+
+public class Q12 {
+    public static void main(String[] args) {
+        Test.show();
+    }
+}
+
