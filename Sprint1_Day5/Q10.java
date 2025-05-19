2f2524b6a3456e3a6b8d9ab45523c3d6dd2fac9f
Aditya Shikhar <adityashikhar7@gmail.com>
2025-05-19 07:11:57 +0530

    .

--- Sprint1_Day5/Q10.java
+++ Sprint1_Day5/Q10.java
@@ -5,18 +5,18 @@ import java.sql.*;
 public class Q10 {
     public static void main(String[] args) {
 
-        // Declare non-resource variables outside
+        
         String url = "jdbc:mysql://localhost:3306/jdbc";
         String username = "root";
         String password = "Shikhar@123";
 
-        // try-with-resources to auto-close JDBC resources
+        
         try (
                 Connection connection = DriverManager.getConnection(url, username, password);
                 Statement statement = connection.createStatement()
         ) {
-            // Intentional SQL error to trigger exception
-            String query = "SELEC * FROM students"; // wrong SELECT
+            
+            String query = "SELEC * FROM students";
             statement.execute(query);
 
         } catch (SQLException e) {
